de107a207c42a67bf4f30357816f6063b5398b90
arnrj010 <arunraj.raj17@gmail.com>
2025-07-04 19:16:35 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,555 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-reception",
+      "name": "reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.04202932986695096,
+        "pitch": 0.0691189422138585,
+        "fov": 1.2947878218179307
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2086805058238603,
+          "pitch": 0.491912226671511,
+          "rotation": 3.141592653589793,
+          "target": "1-corridor"
+        },
+        {
+          "yaw": 0.7977895438982241,
+          "pitch": 0.3146495638841813,
+          "rotation": 3.141592653589793,
+          "target": "2-corridor-01"
+        },
+        {
+          "yaw": 1.4143762570461078,
+          "pitch": 0.2238627510229403,
+          "rotation": 3.141592653589793,
+          "target": "3-mens-section"
+        },
+        {
+          "yaw": 0.6510981308537982,
+          "pitch": 0.09312797536390605,
+          "rotation": 1.5707963267948966,
+          "target": "5-office-corridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-corridor",
+      "name": "corridor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.20480904487687468,
+        "pitch": -3.552713678800501e-15,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22542569414203228,
+          "pitch": 0.3910696019677822,
+          "rotation": 3.141592653589793,
+          "target": "3-mens-section"
+        },
+        {
+          "yaw": 0.18713698026294523,
+          "pitch": -0.029443535394543474,
+          "rotation": 3.141592653589793,
+          "target": "4-display"
+        },
+        {
+          "yaw": -1.1633808085300235,
+          "pitch": 0.5204617489612566,
+          "rotation": 3.141592653589793,
+          "target": "2-corridor-01"
+        },
+        {
+          "yaw": -1.0066121481730192,
+          "pitch": 0.18789847396956638,
+          "rotation": 1.5707963267948966,
+          "target": "5-office-corridor"
+        },
+        {
+          "yaw": 2.735410770505104,
+          "pitch": 0.5180972909973711,
+          "rotation": 3.141592653589793,
+          "target": "0-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-corridor-01",
+      "name": "corridor 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4719667162484331,
+          "pitch": 0.32161727707422116,
+          "rotation": 3.141592653589793,
+          "target": "9-womens-section01"
+        },
+        {
+          "yaw": -0.18867768554869357,
+          "pitch": 0.18183968334181522,
+          "rotation": 3.141592653589793,
+          "target": "10-womens-section02"
+        },
+        {
+          "yaw": -2.17388096786355,
+          "pitch": 0.1855943569102081,
+          "rotation": 3.141592653589793,
+          "target": "0-reception"
+        },
+        {
+          "yaw": 2.3300266320813865,
+          "pitch": 0.3854023180542896,
+          "rotation": 3.141592653589793,
+          "target": "3-mens-section"
+        },
+        {
+          "yaw": 1.8242635948098584,
+          "pitch": 0.09012982365535649,
+          "rotation": 3.141592653589793,
+          "target": "4-display"
+        },
+        {
+          "yaw": 0.5604036768244463,
+          "pitch": 0.26873395223984,
+          "rotation": 1.5707963267948966,
+          "target": "5-office-corridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-mens-section",
+      "name": "mens section",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.17186808213546,
+          "pitch": 0.35664083829567517,
+          "rotation": 3.141592653589793,
+          "target": "2-corridor-01"
+        },
+        {
+          "yaw": 1.4261720873157184,
+          "pitch": 0.19800849596214043,
+          "rotation": 3.141592653589793,
+          "target": "0-reception"
+        },
+        {
+          "yaw": 1.5298350009382133,
+          "pitch": 0.4450874507016209,
+          "rotation": 3.141592653589793,
+          "target": "1-corridor"
+        },
+        {
+          "yaw": -1.4793104605735419,
+          "pitch": -0.053211887063802976,
+          "rotation": 3.141592653589793,
+          "target": "4-display"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-display",
+      "name": "display",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.019279250621956123,
+        "pitch": 0.019427376062029822,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0491915253489168,
+          "pitch": 0.15773717773701001,
+          "rotation": 3.141592653589793,
+          "target": "3-mens-section"
+        },
+        {
+          "yaw": 0.21809889705408558,
+          "pitch": 0.11535954432599382,
+          "rotation": 3.141592653589793,
+          "target": "2-corridor-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-office-corridor",
+      "name": "office corridor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6553889436060416,
+        "pitch": -0.006496218278734034,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4214982656342716,
+          "pitch": 0.21082574483476435,
+          "rotation": 1.5707963267948966,
+          "target": "7-womens-pedicure"
+        },
+        {
+          "yaw": 1.730781313807313,
+          "pitch": 0.3412419345578659,
+          "rotation": 3.141592653589793,
+          "target": "6-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-office",
+      "name": "office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.46190079089448055,
+          "pitch": 0.25782910717475716,
+          "rotation": 3.141592653589793,
+          "target": "5-office-corridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-womens-pedicure",
+      "name": "women's pedicure",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.46111735144419264,
+          "pitch": 0.4539751215632837,
+          "rotation": 3.141592653589793,
+          "target": "8-bridal-makeup"
+        },
+        {
+          "yaw": -0.4073353879458512,
+          "pitch": 0.46729442111062625,
+          "rotation": 4.71238898038469,
+          "target": "5-office-corridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bridal-makeup",
+      "name": "bridal makeup",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9045548359915347,
+          "pitch": 0.6405279869235692,
+          "rotation": 3.141592653589793,
+          "target": "7-womens-pedicure"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-womens-section01",
+      "name": "womens section01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8554984743939045,
+          "pitch": 0.30774133120621805,
+          "rotation": 3.141592653589793,
+          "target": "5-office-corridor"
+        },
+        {
+          "yaw": 2.58272803480106,
+          "pitch": 0.3748152047647615,
+          "rotation": 3.141592653589793,
+          "target": "2-corridor-01"
+        },
+        {
+          "yaw": 0.028745067408017633,
+          "pitch": 0.3370041017804546,
+          "rotation": 3.141592653589793,
+          "target": "10-womens-section02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-womens-section02",
+      "name": "womens section02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5496223806810994,
+        "pitch": 0.0002975291659552681,
+        "fov": 1.4070723946101484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5590654139662359,
+          "pitch": 0.3709815362453117,
+          "rotation": 3.141592653589793,
+          "target": "10-womens-section02"
+        },
+        {
+          "yaw": 1.2879313922302646,
+          "pitch": 0.1672690070602325,
+          "rotation": 3.141592653589793,
+          "target": "2-corridor-01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Saloon 3d edited",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
